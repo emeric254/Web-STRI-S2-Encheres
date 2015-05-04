aef4ada0ed0812c04fb0fc4462255b85d5d64149
emeric254 <emeric254@live.fr>
2015-05-04 11:36:33 +0200

    demarage php

---
+++ PHP/vue/entete.php
@@ -0,0 +1,21 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="fr" style="position: relative; min-height: 100%;">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="author" content="les BG">
+        <meta name="description" content="strideal">
+        <title>STRIDEAL</title>
+        <link rel="icon" href="https://raw.githubusercontent.com/emeric254/Web-STRI-S2-Encheres/master/PHP/favicon.ico">
+        <!-- Bootstrap -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
+        <!-- Font-Awesome -->
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    </head>
+    
+    <body role="document" style="padding-top: 20px; padding-bottom: 20px; margin-bottom: 30px; min-width: 240px;">
---
+++ PHP/vue/footer.php
@@ -0,0 +1,32 @@
+<?php
+
+?>
+        <!-- Footer -->
+        <footer class="footer" style="position: absolute; bottom: 0; width: 100%; height: 30px; background-color: #d0d0d0;">
+            <div class="container">
+                <p class="pull-left">
+                    <span class="label">
+                        <a href="apropos.html">
+                            <i class="fa fa-html5"></i>
+							&nbsp;
+                            <em>STRIDEAL</em>, sous license <em>GNU G.P.L. v2</em>
+                        </a>
+                    </span>
+                </p>
+                <p class="pull-right">
+                    <span class="label label-info">
+                        <a href="#">
+                            <i class="fa fa-chevron-circle-up"></i>
+                            Retour en haut
+                        </a>
+                    </span>
+                </p>
+            </div>
+        </footer>
+
+        <!-- JQuery -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <!-- Bootstrap -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+    </body>
+</html>
