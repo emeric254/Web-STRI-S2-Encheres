ef46eaf9bd21016edb3e3dcbecc7360866f45e30
emeric254 <emeric254@live.fr>
2015-05-07 09:18:39 +0200

    maj vue vente

--- PHP/vue/vente.php
+++ PHP/vue/vente.php
@@ -3,7 +3,14 @@
  * page affichant les details d'une vente
  * 
  * vars
- * //@TODO
+ * $titreVente
+ * $tempsVente
+ * $prixVente
+ * $nbEnchereVente
+ * $photoVente
+ * $descriptionVente
+ * 
+ * $encherisseursVente
  * 
  */
 ?>
@@ -11,32 +18,35 @@
 				<div class="page-header">
 					<div class="row">
 						<h1>
-							Titre Vente
+							<?php print $titreVente; ?>
 						</h1>
 						<div class="col-sm-6 col-md-4 col-lg-4">
 							<h3>
 								<span class="label label-warning">
-									2 minutes
+									<?php print $tempsVente; ?>
 								</span>
 								
 								&nbsp;
 								<span class="label label-info">
-									4000$
-									<span class="badge">42</span>
+									<?php print $prixVente; ?>
+									€
+									<span class="badge">
+										<?php print $nbEnchereVente; ?>
+									</span>
 								</span>
 							</h3>
 						</div>
 						<div class="col-md-4 col-lg-5">
 						</div>
 						<div class="col-sm-6 col-md-4 col-lg-3">
-							<img data-src="holder.js/200x200" class="img-thumbnail" alt="200x200" src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iMjAwIiBoZWlnaHQ9IjIwMCIgdmlld0JveD0iMCAwIDIwMCAyMDAiIHByZXNlcnZlQXNwZWN0UmF0aW89Im5vbmUiPjxkZWZzLz48cmVjdCB3aWR0aD0iMjAwIiBoZWlnaHQ9IjIwMCIgZmlsbD0iI0VFRUVFRSIvPjxnPjx0ZXh0IHg9Ijc1IiB5PSIxMDAiIHN0eWxlPSJmaWxsOiNBQUFBQUE7Zm9udC13ZWlnaHQ6Ym9sZDtmb250LWZhbWlseTpBcmlhbCwgSGVsdmV0aWNhLCBPcGVuIFNhbnMsIHNhbnMtc2VyaWYsIG1vbm9zcGFjZTtmb250LXNpemU6MTBwdDtkb21pbmFudC1iYXNlbGluZTpjZW50cmFsIj4yMDB4MjAwPC90ZXh0PjwvZz48L3N2Zz4=" data-holder-rendered="true" style="width: 200px; height: 200px;">
+							<img data-src="holder.js/200x200" class="img-thumbnail" alt="200x200" src="<?php print $photoVente; ?>" data-holder-rendered="true" style="width: 200px; height: 200px;">
 						</div>
 					</div>
 				</div>
 				<div class="row">
 					<div class="well">
 						<p>
-							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed doeiusmod tempor incididunt ut labore et dolore magna aliqua.Excepteur sint occaecat cupidatat non proident, sunt inculpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed doeiusmod tempor incididunt ut labore et dolore magna aliqua.
+							<?php print $descriptionVente; ?>
 						</p>
 					</div>
 				</div>
@@ -63,51 +73,30 @@
 							</tr>
 						</thead>
 						<tbody>
+<?php
+//foreach(encherisseursVente)
+/*
+?>							
 							<tr>
 								<td>
-									Mark
+									<?php print $nom; ?>
 								</td>
 								<td>
 									<span class="label label-info">
-										4000$
+										<?php print $prix; ?>
+										€
 									</span>
 								</td>
 								<td>
 									<span class="label label-warning">
-										2 minutes
+										<?php print $temps; ?>
 									</span>
 								</td>
 							</tr>
-							<tr>
-								<td>
-									Jacob-Kevin
-								</td>
-								<td>
-									<span class="label label-info">
-										2000$
-									</span>
-								</td>
-								<td>
-									<span class="label label-warning">
-										4 minutes
-									</span>
-								</td>
-							</tr>
-							<tr>
-								<td>
-									Larry
-								</td>
-								<td>
-									<span class="label label-info">
-										20$
-									</span>
-								</td>
-								<td>
-									<span class="label label-warning">
-										42 minutes
-									</span>
-								</td>
-							</tr>
+<?php
+*/
+//
+?>							
 						</tbody>
 					</table>
 				</div>
