62cb0380365cfed911e18c0200c316b1f771c184
Kriss <boulic.guillaume@gmail.com>
2015-05-09 20:38:16 +0200

    correction

--- PHP/core/model.php
+++ PHP/core/model.php
@@ -66,9 +66,9 @@ function NavbarCheckInfo($id,$user,$pass) {
  */
 function Profil_Info_Compte($mail) {
 	include('core/bdd.php');
-	$req = "SELECT * FROM utilisateur WHERE emailutilisateur=\'$mail\';";
+	$req = "SELECT * FROM utilisateur WHERE emailutilisateur='?';";
 	$reqExec = $db->prepare($req);
-	$reqExec->execute(array());
+	$reqExec->execute(array($mail));
 	
 	while ($donnees_reqExec = $reqExec->fetch()){
 		$ret['prenomClient'] = $donnees_reqExec['nomutilisateur'];
