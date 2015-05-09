f1e166776a2ee4e2f364656062105332aac43892
Kriss <boulic.guillaume@gmail.com>
2015-05-09 15:01:29 +0200

    Ajut de la fonction de récupération des info général d'une annonce

--- PHP/core/controleur.php
+++ PHP/core/controleur.php
@@ -18,3 +18,20 @@
  */
 session_start();
 include('core/bdd.php');
+
+# -------------- Fonction controleur pour vente.php
+
+function Vente_Info_General($id) {
+	$req = "SELECT * FROM annonce WHERE idannonce=?";
+	$reqExec = $db->prepare($req);
+	$reqExec->execute(array($id));
+	while ($donnees_reqExec = $reqExec->fetch()){
+		$ret['titreVente'] = $donnees_reqExec['nomannonce'];
+		$ret['tempsVente'] = $donnees_reqExec['dureeannonce'];
+		$ret['prixVente'] = $donnees_reqExec['prixdepartannonce'];
+		$ret['photoVente'] = $donnees_reqExec['urlphotoannonce'];
+		$ret['descriptionVente'] = $donnees_reqExec['descriptionannonce'];
+	}
+	return $ret;
+}
+?>
