ac0777fd9fcf031fac40dff7d0ba23d09f77cd43
Kriss <boulic.guillaume@gmail.com>
2015-05-09 16:06:03 +0200

    controleur pour la navbar

---
+++ PHP/core/navbar.php
@@ -0,0 +1,41 @@
+<?php
+/* «core/navbar.php»
+ * barre de menu sur toutes les pages du site
+ * 
+ * vars
+ * $connecte  (represente l'etat de connexion de la personne sur le site, connectee ou non)
+ * 
+ * 
+ */
+include('core/model.php');
+if(isset($_SESSION['id']) and !empty($_SESSION['id']) and isset($_SESSION['user']) and !empty($_SESSION['user']) and isset($_SESSION['pass']) and !empty($_SESSION['pass'])) {
+	$id = htmlspecialchars($_SESSION['id']);
+	$user = htmlspecialchars($_SESSION['user']);
+	$pass = htmlspecialchars($_SESSION['pass']);
+	if(NavbarCheckInfo($id,$user,$pass)){
+		$connecte = true;
+	} else {
+		$connecte = false;
+		unset($_SESSION['id']);
+		unset($_SESSION['user']);
+		unset($_SESSION['pass']);
+	}
+} else if (isset($_COOKIE['id']) and !empty($_COOKIE['id']) and isset($_COOKIE['user']) and !empty($_COOKIE['user']) and isset($_COOKIE['pass']) and !empty($_COOKIE['pass'])) {
+	$id = htmlspecialchars($_COOKIE['id']);
+	$user = htmlspecialchars($_COOKIE['user']);
+	$pass = htmlspecialchars($_COOKIE['pass']);
+	if(NavbarCheckInfo($id,$user,$pass)){
+		$connecte = true;
+		$_SESSION['id'];
+		$_SESSION['user'];
+		$_SESSION['pass'];
+	} else {
+		$connecte = false;
+		unset($_COOKIE['id']);
+		unset($_COOKIE['user']);
+		unset($_COOKIE['pass']);
+	}
+} else {
+	$connecte = false;
+}
+?>
