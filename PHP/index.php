c678222fc82f82684f9d30a1eca6f4a9cda2582b
Kriss <boulic.guillaume@gmail.com>
2015-05-09 18:35:02 +0200

    modification de la page index

--- PHP/index.php
+++ PHP/index.php
@@ -1,11 +1,17 @@
-<?php
-
-?>
 
 <?php
 	include("vue/entete.php");
 	
 	include("vue/navbar.php");
-	
+	if ( isset($_GET['page']) and !empty($_GET['page']) ) {
+		$page=htmlspecialchars($_GET['page']);
+	} else {
+		$page="nouveautes"; //page par default
+	}
+	if(file_exists("vue/$page.php")){
+		include_once("vue/$page.php")
+	} else {
+		include_once("vue/404.php"); //page d'erreur'
+	}
 	include("vue/footer.php");
 ?>
