8756d0b2211bae7f41d6aad9e17a907689469ddd
emeric254 <emeric254@live.fr>
2015-05-10 16:58:33 +0200

    vue erreur.php

---
+++ PHP/vue/erreur.php
@@ -0,0 +1,22 @@
+<?php
+/* «erreur.php»
+ * page erreur sur le site
+ * 
+ * vars
+ * > $errMsg	message d'erreur
+ * 
+ */
+?>
+        <!-- Titre -->
+        <div class="jumbotron">
+            <div class="container theme-showcase" role="main" >
+                <h1 class="text-center">
+                    Erreur !
+                </h1>
+                <div class="alert alert-danger" role="alert">
+					<span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+					<span class="sr-only">Error:</span>
+					<?php print $errMsg; ?>
+				</div>
+            </div>
+        </div>
