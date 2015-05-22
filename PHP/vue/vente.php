b42bd8ce082c5b5286c5fd3a519af520fe7dd726
emeric254 <emeric254@live.fr>
2015-05-22 11:46:21 +0200

    debug a finir de vente .php

--- PHP/vue/vente.php
+++ PHP/vue/vente.php
@@ -1,7 +1,7 @@
 <?php
 /* «vente.php»
  * page affichant les details d'une vente
- * 
+ *
  * vars
  * $titreVente
  * $tempsVente
@@ -9,97 +9,99 @@
  * $nbEnchereVente
  * $photoVente
  * $descriptionVente
- * 
+ *
  * $encherisseursVente (tab des encherisseurs)
- * 
+ *
  * //@TODO commentaires ?
- * 
+ *
  */
 ?>
-			<div class="jumbotron">
-				<div class="page-header">
-					<div class="row">
-						<h1>
-							<?php print $titreVente; ?>
-						</h1>
-						<div class="col-sm-6 col-md-4 col-lg-4">
-							<h3>
-								<span class="label label-warning">
-									<?php print $tempsVente; ?>
-								</span>
-								
-								&nbsp;
-								<span class="label label-info">
-									<?php print $prixVente; ?>
-									€
-									<span class="badge">
-										<?php print $nbEnchereVente; ?>
-									</span>
-								</span>
-							</h3>
-						</div>
-						<div class="col-md-4 col-lg-5">
-						</div>
-						<div class="col-sm-6 col-md-4 col-lg-3">
-							<img data-src="holder.js/200x200" class="img-thumbnail" alt="200x200" src="<?php print $photoVente; ?>" data-holder-rendered="true" style="width: 200px; height: 200px;">
-						</div>
-					</div>
-				</div>
-				<div class="row">
-					<div class="well">
-						<p>
-							<?php print $descriptionVente; ?>
-						</p>
-					</div>
-				</div>
-			</div>
-			<div class="container">
-				<div class="col-md-5 col-lg-5">
-					<h2>
-						Derniers encherisseurs :
-					</h2>
-				</div>
-				<div class="col-md-7 col-lg-7">
-					<table class="table table-bordered">
-						<thead>
-							<tr>
-								<th>
-									Nom
-								</th>
-								<th>
-									Enchere
-								</th>
-								<th>
-									Depuis
-								</th>
-							</tr>
-						</thead>
-						<tbody>
+            <div class="jumbotron">
+                <h1>
+                    <?php print $titreVente; ?>
+                </h1>
+            </div>
+
+            <div class="wells">
+                <div class="page-header">
+                    <div class="col-sm-6 col-md-4 col-lg-4">
+                        <h3>
+                            <span class="label label-warning">
+                                <?php print $tempsVente; ?>
+                            </span>
+
+                            &nbsp;
+                            <span class="label label-info">
+                                <?php print $prixVente; ?>
+                                €
+                                <span class="badge">
+                                    <?php print $nbEnchereVente; ?>
+                                </span>
+                            </span>
+                        </h3>
+                    </div>
+                    <div class="col-md-4 col-lg-5">
+                    </div>
+                    <div class="col-sm-6 col-md-4 col-lg-3">
+                        <img data-src="holder.js/200x200" class="img-thumbnail" alt="200x200" src="<?php print $photoVente; ?>" data-holder-rendered="true" style="width: 200px; height: 200px;">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="well">
+                        <p>
+                            <?php print $descriptionVente; ?>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="container">
+                <div class="col-md-5 col-lg-5">
+                    <h2>
+                        Derniers encherisseurs :
+                    </h2>
+                </div>
+                <div class="col-md-7 col-lg-7">
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th>
+                                    Nom
+                                </th>
+                                <th>
+                                    Enchere
+                                </th>
+                                <th>
+                                    Depuis
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
 <?php
 //foreach(encherisseursVente)
 /*
-?>							
-							<tr>
-								<td>
-									<?php print $nom; ?>
-								</td>
-								<td>
-									<span class="label label-info">
-										<?php print $prix; ?>
-										€
-									</span>
-								</td>
-								<td>
-									<span class="label label-warning">
-										<?php print $temps; ?>
-									</span>
-								</td>
-							</tr>
+?>
+                            <tr>
+                                <td>
+                                    <?php print $nom; ?>
+                                </td>
+                                <td>
+                                    <span class="label label-info">
+                                        <?php print $prix; ?>
+                                        €
+                                    </span>
+                                </td>
+                                <td>
+                                    <span class="label label-warning">
+                                        <?php print $temps; ?>
+                                    </span>
+                                </td>
+                            </tr>
 <?php
 */
 //
-?>							
-						</tbody>
-					</table>
-				</div>
-			</div>
+?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
