13c453a461f161d795b6b3e96b5ceff331e0fc7b
emeric254 <emeric254@live.fr>
2015-05-23 14:27:46 +0200

    maj profil.php

--- PHP/vue/profil.php
+++ PHP/vue/profil.php
@@ -1,80 +1,78 @@
 <?php
 /* «profil.php»
  * page affichant le profil d'un utilisateur
- * 
+ *
  * vars
- * $profil	(le profil de l'utilisateur)
+ * $profil  (le profil de l'utilisateur)
  *
  */
 ?>
-
         <div class="container">
-			
-			<div class="jumbotron">
-				
-				<div class="page-header">
-					<div class="row">
-						
-						<div class="col-sm-6 col-md-6 col-lg-7">
-							<h1>
-								<?php print "$profil->prenom $profil->nom"; ?>
-							</h1>
-						</div>
-						
-						<div class="col-sm-1 col-md-2 col-lg-2">
-						</div>
-						
-						<div class="col-sm-5 col-md-4 col-lg-3">
-							<img data-src="holder.js/200x200" class="img-thumbnail" alt="200x200" src="profil->photo" data-holder-rendered="true" style="width: 200px; height: 200px;">
-						</div>
-						
-					</div>
-				</div>
-				
-				<div class="row">
-					<div class="well">
-						<table class="table table-bordered">
-							<thead>
-								<tr>
-									<td colspan="2" class="text-center">
-										<b>
-											Informations personelles
-										</b>
-									</td>
-								</tr>
-							</thead>
-							<tbody>
-								<tr>
-									<td class="text-center">
-										Email
-									</td>
-									<td class="text-center">
-										<?php print $profil->email; ?>
-									</td>
-								</tr>
-								<tr>
-									<td class="text-center">
-										Telephone
-									</td>
-									<td class="text-center">
-										<?php print $profil->telephone; ?>
-									</td>
-								</tr>
-								<tr>
-									<td class="text-center">
-										Adresse
-									</td>
-									<td class="text-center">
-										<?php print $profil->adresse; ?>,
-										<?php print $profil->ville; ?>
-									</td>
-								</tr>
-							</tbody>
-						</table>
-					</div>
-				</div>
-				
-			</div>
-			
-        </div>
 
+            <div class="jumbotron">
+
+                <div class="page-header">
+                    <div class="row">
+
+                        <div class="col-sm-6 col-md-6 col-lg-7">
+                            <h1>
+                                <?php print "$profil->prenom $profil->nom"; ?>
+                            </h1>
+                        </div>
+
+                        <div class="col-sm-1 col-md-2 col-lg-2">
+                        </div>
+
+                        <div class="col-sm-5 col-md-4 col-lg-3">
+                            <img data-src="holder.js/200x200" class="img-thumbnail" alt="200x200" src="<?php print "$profil->photo"; ?>" data-holder-rendered="true" style="width: 200px; height: 200px;">
+                        </div>
+
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="well">
+                        <table class="table table-bordered">
+                            <thead>
+                                <tr>
+                                    <td colspan="2" class="text-center">
+                                        <b>
+                                            Informations personelles
+                                        </b>
+                                    </td>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td class="text-center">
+                                        Email
+                                    </td>
+                                    <td class="text-center">
+                                        <?php print $profil->email; ?>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class="text-center">
+                                        Telephone
+                                    </td>
+                                    <td class="text-center">
+                                        <?php print $profil->telephone; ?>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class="text-center">
+                                        Adresse
+                                    </td>
+                                    <td class="text-center">
+                                        <?php print $profil->adresse; ?>,
+                                        <?php print $profil->ville; ?>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
