a4badfc42e554e850b89ffa90640b925c675b44f
emeric254 <emeric254@live.fr>
2015-05-23 20:21:40 +0200

    maj vue error.php

--- PHP/vue/erreur.php
+++ PHP/vue/erreur.php
@@ -1,10 +1,10 @@
 <?php
 /* «erreur.php»
  * page erreur sur le site
- * 
+ *
  * vars
- * > $errMsg	message d'erreur
- * 
+ * > $errMsg    le message d'erreur a afficher
+ *
  */
 ?>
         <!-- Titre -->
@@ -14,9 +14,9 @@
                     Erreur !
                 </h1>
                 <div class="alert alert-danger" role="alert" style="margin-top:30px;">
-					<span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
-					<span class="sr-only">Error:</span>
-					<?php print $errMsg; ?>
-				</div>
+                    <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+                    <span class="sr-only">Message d'erreur : </span>
+                    <?php print($errMsg); ?>
+                </div>
             </div>
         </div>
