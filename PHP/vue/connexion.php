472e3b7e1815c6f607c5e0afa86ce38acbae1ab1
emeric254 <emeric254@live.fr>
2015-05-23 20:17:43 +0200

    maj vue connexion.php

--- PHP/vue/connexion.php
+++ PHP/vue/connexion.php
@@ -1,55 +1,59 @@
 <?php
 /* «connexion.php»
  * simple formulaire de connexion
- * 
- * //@TODO  action form a changer
- * 
+ *
+ * //@TODO  action du form a changer
+ *
+ * inputs :
+ *  > inputEmail
+ *  > inputPassword
+ *
  */
 ?>
         <!-- Titre -->
         <div class="jumbotron">
             <div class="container theme-showcase" role="main" >
                 <h1>
-					<i class="fa fa-sign-in"></i>
-					Connexion
+                    <i class="fa fa-sign-in"></i>
+                    Connexion
                 </h1>
             </div>
         </div>
-        
+
         <!-- Contenu -->
         <div class="container">
-			
-			<!-- Formulaire -->
-			<form class="form-signin" action="core/traitementConnexion.php" method="POST">
-				
-				<div class="well">
-					<!-- Email -->
-					<div class="input-group input-group-lg">
-						<span class="input-group-addon">
-							@
-						</span>
-						<label for="inputEmail" class="sr-only">
-							Adresse Email
-						</label>
-						<input type="email" id="inputEmail" name="email" class="form-control" placeholder="Email" required autofocus>
-					</div>
-				</div>
-				
-				<div class="well">
-					<!-- Password -->
-					<div class="input-group input-group-lg">
-						<span class="input-group-addon">
-							*
-						</span>
-						<label for="inputPassword" class="sr-only">
-							Mot de passe
-						</label>
-						<input type="password" id="inputPassword" name="pwd" class="form-control" placeholder="Mot de passe" required>
-					</div>
-				</div>
-				
-				<!-- Submit -->
-				<button class="btn btn-lg btn-primary btn-block" type="submit">Connexion</button>
-				
-			</form>
+
+            <!-- Formulaire -->
+            <form class="form-signin" action="index.php" method="POST">
+
+                <div class="well">
+                    <!-- Email -->
+                    <div class="input-group input-group-lg">
+                        <span class="input-group-addon">
+                            @
+                        </span>
+                        <label for="inputEmail" class="sr-only">
+                            Adresse Email
+                        </label>
+                        <input type="email" id="inputEmail" name="email" class="form-control" placeholder="Email" required autofocus>
+                    </div>
+                </div>
+
+                <div class="well">
+                    <!-- Password -->
+                    <div class="input-group input-group-lg">
+                        <span class="input-group-addon">
+                            *
+                        </span>
+                        <label for="inputPassword" class="sr-only">
+                            Mot de passe
+                        </label>
+                        <input type="password" id="inputPassword" name="pwd" class="form-control" placeholder="Mot de passe" required>
+                    </div>
+                </div>
+
+                <!-- Submit -->
+                <button class="btn btn-lg btn-primary btn-block" type="submit">Connexion</button>
+
+            </form>
         </div>
