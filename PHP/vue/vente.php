bec34cfa4e12d87744b549abdb85c92cd383be36
emeric254 <emeric254@live.fr>
2015-05-23 15:09:47 +0200

    maj vente, tab et comments

--- PHP/vue/vente.php
+++ PHP/vue/vente.php
@@ -2,12 +2,12 @@
 /* «vente.php»
  * page affichant les details d'une vente
  *
- * 
+ *
  * vars :
- * 	> $vente	(objet "vente"
- * 
+ *  > $vente    (objet "vente"
+ *
  * // @ TODO pour une evolve future :
- * 	> $encherisseursVente (tableau de "profil")
+ *  > $encherisseursVente (tableau de "profil")
  *
  */
 ?>
@@ -20,56 +20,56 @@
             <div class="well">
                 <div class="row text-center">
                     <div class="col-md-6 col-lg-6 text-center">
-						<h3>
-							<span class="label label-danger" id="tempsRestant">
-								<!-- temps restant -->
-								0
-							</span>
-							&nbsp;
-						</h3>
-						<h3>
-							<span class="label label-info">
-								Enchère crée par
-								<a href="/?page=profil&id=<?php print $vente->Vendeur->id; ?>">
-									<?php print $vente->Vendeur->nom; ?>
-								</a>
-							</span>
-							&nbsp;
-						</h3>
-					</div>
+                        <h3>
+                            <span class="label label-danger" id="tempsRestant">
+                                <!-- temps restant -->
+                                0
+                            </span>
+                            &nbsp;
+                        </h3>
+                        <h3>
+                            <span class="label label-info">
+                                Enchère crée par
+                                <a href="/?page=profil&id=<?php print $vente->Vendeur->id; ?>">
+                                    <?php print $vente->Vendeur->nom; ?>
+                                </a>
+                            </span>
+                            &nbsp;
+                        </h3>
+                    </div>
                     <div class="col-md-6 col-lg-6 text-center">
-						<h3>
-							<span class="label label-warning">
-								<?php print $vente->prix; ?>
-								€
-								<span class="badge">
-									<?php print $vente->nbEncherisseur; ?>
-								</span>
-							</span>
-						</h3>
-						<h3>
-							<span class="label label-info">
-								Dernière enchère par
-								<a href="?page=profil&id=<?php print $vente->Acheteur->id; ?>">
-									<?php print $vente->Acheteur->nom; ?>
-								</a>
-							</span>
-							&nbsp;
-						</h3>
-					</div>
+                        <h3>
+                            <span class="label label-warning">
+                                <?php print $vente->prix; ?>
+                                €
+                                <span class="badge">
+                                    <?php print $vente->nbEncherisseur; ?>
+                                </span>
+                            </span>
+                        </h3>
+                        <h3>
+                            <span class="label label-info">
+                                Dernière enchère par
+                                <a href="?page=profil&id=<?php print $vente->Acheteur->id; ?>">
+                                    <?php print $vente->Acheteur->nom; ?>
+                                </a>
+                            </span>
+                            &nbsp;
+                        </h3>
+                    </div>
                 </div>
                 <div class="row">
                     <div class="col-sm-5 col-md-4 col-lg-4 text-center">
                         <img data-src="holder.js/200x200" class="img-thumbnail" alt="200x200" src="<?php print $vente->photo; ?>" data-holder-rendered="true" style="width: 200px; height: 200px;">
                     </div>
-					<div class="col-sm-7 col-md-8 col-lg-8">
-						<div style="padding-top: 10px;">
-							<div class="well">
-								<p>
-									<?php print $vente->description; ?>
-								</p>
-							</div>
-						</div>
+                    <div class="col-sm-7 col-md-8 col-lg-8">
+                        <div style="padding-top: 10px;">
+                            <div class="well">
+                                <p>
+                                    <?php print $vente->description; ?>
+                                </p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -132,39 +132,44 @@ foreach($encherisseursVente as $profil)
 */
 ?>
 
+
+
+<!--
+Script de decompte pour le temps restant !
+-->
+
 <script type="text/javascript">
-	
-	function decompte()
-	{	
-		var dateActuelle = new Date();
-		
-		// @ TODO a verif que ca marche de creer la date de debut comme ca !
-		var dateDebut = new Date("<?php print $vente->date; ?>");
-		
-		// @ TODO a verif que ce soit des secondes !
-		var duree = <?php print $vente->tempsRestant; ?>;
-		
-		var total = duree - (dateActuelle - dateDebut)/1000 ;
-		
-		var compteRebour = document.getElementById("tempsRestant");
-		
-		if (total <= 0)
-		{
-			compteRebour.innerHTML = 'Finie !';
-		}
-		else
-		{
-			var jours = Math.floor(total / (60 * 60 * 24));
-			var heures = Math.floor((total - (jours * 60 * 60 * 24)) / (60 * 60));
-			var minutes = Math.floor((total - ((jours * 60 * 60 * 24 + heures * 60 * 60))) / 60);
-			var secondes = Math.floor(total - ((jours * 60 * 60 * 24 + heures * 60 * 60 + minutes * 60)));
-			
-			compteRebour.innerHTML = jours + 'j ' + heures + 'h ' + minutes + 'm ' + secondes + 's';
-		}
-			setTimeout("decompte();", 1000);
-	}
-	
-	decompte();
+
+    function decompte()
+    {
+        var dateActuelle = new Date();
+
+        // @ TODO a verif que ca marche de creer la date de debut comme ca !
+        var dateDebut = new Date("<?php print $vente->date; ?>");
+
+        // @ TODO a verif que ce soit des secondes !
+        var duree = <?php print $vente->tempsRestant; ?>;
+
+        var total = duree - (dateActuelle - dateDebut)/1000 ;
+
+        var compteRebour = document.getElementById("tempsRestant");
+
+        if (total <= 0)
+        {
+            compteRebour.innerHTML = 'Finie !';
+        }
+        else
+        {
+            var jours = Math.floor(total / (60 * 60 * 24));
+            var heures = Math.floor((total - (jours * 60 * 60 * 24)) / (60 * 60));
+            var minutes = Math.floor((total - ((jours * 60 * 60 * 24 + heures * 60 * 60))) / 60);
+            var secondes = Math.floor(total - ((jours * 60 * 60 * 24 + heures * 60 * 60 + minutes * 60)));
+
+            compteRebour.innerHTML = jours + 'j ' + heures + 'h ' + minutes + 'm ' + secondes + 's';
+        }
+            setTimeout("decompte();", 1000);
+    }
+
+    decompte();
 
 </script>
-
