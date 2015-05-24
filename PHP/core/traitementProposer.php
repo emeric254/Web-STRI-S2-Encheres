984fe24fb39d74d7eca67d0aab187325572a2cd1
remi <remi.b7@hotmail.fr>
2015-05-24 20:19:55 +0200

    recupération des varaiables du formulaire

--- PHP/core/traitementProposer.php
+++ PHP/core/traitementProposer.php
@@ -7,7 +7,7 @@
  *
  * TODO: 
  * - Voir si il faut tester que la personne est connectée (a rajouter tout en haut de la page)
- *
+ * - ///!!\\\ => le inputUnite peut pposer probleme : emeric m'a dit de le mettre mais je le voit pas dans le formulaire
  *
  *
  */
@@ -35,22 +35,59 @@ if (isser($_POST['inputDescription']) and !empty($_POST['inputDescription'])) {
     $champErreur.="Description manquante";
 }
 
-if (isser($_POST['inputDescription']) and !empty($_POST['inputDescription'])) {
-    $titre=htmlspecialchars($_POST['inputDescription']);
+if (isser($_POST['inputPrix']) and !empty($_POST['inputPrix'])) {
+    $titre=htmlspecialchars($_POST['inputPrix']);
 }else{
     if($erreur){
 		$champErreur=", ";
 	}
     $erreur=1;
-    $champErreur.="Description manquante";
+    $champErreur.="Veuillez saisir le prix";
 }
 
+if (isser($_POST['inputPas']) and !empty($_POST['inputPas'])) {
+    $titre=htmlspecialchars($_POST['inputPas']);
+}else{
+    if($erreur){
+		$champErreur=", ";
+	}
+    $erreur=1;
+    $champErreur.="Veuillez indiquer le pas";
+}
 
-inputTitre ok
-inputDescription
-inputPrix
-inputPas
-inputUnite
-inputDureeJour
-inputDureeHeure
-inputDureeMinute
+if (isser($_POST['inputDureeJour']) and !empty($_POST['inputDureeJour'])) {
+    $titre=htmlspecialchars($_POST['inputDureeJour']);
+}else{
+    if($erreur){
+		$champErreur=", ";
+	}
+    $erreur=1;
+    $champErreur.="Veuillez saisir nombre de jour";
+}
+
+if (isser($_POST['inputDureeHeure']) and !empty($_POST['inputDureeHeure'])) {
+    $titre=htmlspecialchars($_POST['inputDureeHeure']);
+}else{
+    if($erreur){
+		$champErreur=", ";
+	}
+    $erreur=1;
+    $champErreur.="Veuillez saisir le nombre d'heure";
+}
+
+if (isser($_POST['inputDureeMinute']) and !empty($_POST['inputDureeMinute'])) {
+    $titre=htmlspecialchars($_POST['inputDureeMinute']);
+}else{
+    if($erreur){
+		$champErreur=", ";
+	}
+    $erreur=1;
+    $champErreur.="Veuillez saisir le nombre de minute";
+}
+
+if ($erreur == 1)
+{
+    $errMsg="Veuillez v&eacute;rifier les erreurs suivante : $champErreur.";
+    include_once("vue/erreur.php");
+    include_once("vue/inscription.php");
+}else{
