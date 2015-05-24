ab6f079266de91f49956613c442fbebff5fb03d6
emeric254 <emeric254@live.fr>
2015-05-24 22:55:48 +0200

    proposer controleur ameliorer

--- PHP/core/traitementProposer.php
+++ PHP/core/traitementProposer.php
@@ -35,6 +35,11 @@ $champErreur = " ";
 
 if (isser($_POST['inputTitre']) and !empty($_POST['inputTitre'])) {
     $titre=htmlspecialchars($_POST['inputTitre']);
+    if( strlen($titre) < 1)
+    {
+    $erreur=1;
+    $champErreur="Titre d'annonce invalide";
+    }
 }else{
     $erreur=1;
     $champErreur="Titre d'annonce manquant";
@@ -42,6 +47,14 @@ if (isser($_POST['inputTitre']) and !empty($_POST['inputTitre'])) {
 
 if (isser($_POST['inputDescription']) and !empty($_POST['inputDescription'])) {
     $description=htmlspecialchars($_POST['inputDescription']);
+    if(strlen($description) < 1)
+    {
+        if($erreur){
+        $champErreur=", ";
+        }
+    $erreur=1;
+    $champErreur.="Description invalide";
+    }
 }else{
     if($erreur){
         $champErreur=", ";
@@ -52,6 +65,14 @@ if (isser($_POST['inputDescription']) and !empty($_POST['inputDescription'])) {
 
 if (isser($_POST['inputPrix']) and !empty($_POST['inputPrix'])) {
     $prix=htmlspecialchars($_POST['inputPrix']);
+    if($prix < 1)
+    {
+        if($erreur){
+            $champErreur=", ";
+        }
+        $erreur=1;
+        $champErreur.="Veuillez saisir un prix valide";
+    }
 }else{
     if($erreur){
         $champErreur=", ";
@@ -62,6 +83,14 @@ if (isser($_POST['inputPrix']) and !empty($_POST['inputPrix'])) {
 
 if (isser($_POST['inputPas']) and !empty($_POST['inputPas'])) {
     $pas=htmlspecialchars($_POST['inputPas']);
+    if($pas < 1)
+    {
+        if($erreur){
+            $champErreur=", ";
+        }
+        $erreur=1;
+        $champErreur.="Veuillez indiquer un pas valide";
+    }
 }else{
     if($erreur){
         $champErreur=", ";
@@ -100,24 +129,35 @@ if (isser($_POST['inputDureeMinute']) and !empty($_POST['inputDureeMinute'])) {
     $champErreur.="Veuillez saisir le nombre de minute";
 }
 
+if( !erreur && !($dureeJour > 0 || $dureeHeure > 0 || $dureeMinute > 0) )
+{
+    if($erreur)
+        $champErreur=", ";
+    $erreur=1;
+    $champErreur.="Veuillez saisir une durée valide !";
+}
+
+
 if ($erreur == 1)
 {
     $errMsg="Veuillez v&eacute;rifier les erreurs suivante : $champErreur.";
     include_once("vue/erreur.php");
     include_once("vue/inscription.php");
-}else{
+}else
+{
+    // a regarder jai mis les verif avant donc de tt manieres ce test passe dans tous les cas 
     if ( VerificationInformationAnnonce($titre,$description,$prix,$pas,$dureeJour,$dureeHeure,$dureeMinute) )
     {
-        // @ TODO ... c'est bon
+        // @ TODO c'est bon
         AjoutNouvelleAnnonce($titre,$description,$prix,$pas,$dureeJour,$dureeHeure,$dureeMinute);
     }
     else
     {
-        // @ TODO ... c'est qqch de foireux dans les data rentrée par l'user >>> remettre la page «proposer»
+        // @ TODO  c'est qqch de foireux dans les data rentrée par l'user >>> remettre la page «proposer», NORMALEMENT ON PASSE JAMAIS ICI SAUF BUG MAJEUR DE PHP ... !
     }
 
 
-    // @ TODO a continuer ... remi c'est chauffer pour ce qui suit !? !? !?
+    // @ TODO a continuer ... remi s'est chauffer pour ce qui suit !? !? !?
 
     //ajout de l'objet dans la base de donnée
     // on recherche les informations de l'objet dans la base
