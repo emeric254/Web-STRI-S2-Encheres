990a5fb4cc559d1651c7ccb7beee43bb234e2367
emeric254 <emeric254@live.fr>
2015-05-25 22:53:12 +0200

    retry avec histo bash

--- PHP/core/accueil.php
+++ PHP/core/accueil.php
@@ -1,17 +1,17 @@
 <?php
 /* «nouveautes.php»
  * Page d'acceuil
- * 
+ *
  */
 
 include_once('core/class.php');
 include_once('core/model.php');
 include_once('vue/debut-accueil.php');
-$ListNouv=RecuperationTendanceVente(4);
+$ListNouv=RecuperationDerniereVente(4);
 foreach($ListNouv as $idVente){
-	$vente = new Vente($idVente);
-	include('vue/resume-article.php');
-	unset($vente);
+    $vente = new Vente($idVente);
+    include('vue/resume-article.php');
+    unset($vente);
 }
 include_once('vue/fin-contenu.php');
 
