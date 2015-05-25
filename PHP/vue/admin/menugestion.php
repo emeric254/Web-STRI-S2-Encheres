e90297b3b3a2522cdb9655946fe04db31dc74a0d
emeric254 <emeric254@live.fr>
2015-05-25 15:27:20 +0200

    maj gestion admin

---
+++ PHP/vue/admin/menugestion.php
@@ -0,0 +1,49 @@
+<?php
+/* «menu-gestion.php»
+ * page d'accueil pour la gestion du site par un admin
+ * 
+ * 
+ */
+?>
+        <!-- Contenu -->
+        <div class="container">
+        
+            <div class="well">
+            
+                <h2 style="text-align:center;">
+                    Mon compte
+                </h2>
+                <p></p>
+                <ul class="nav nav-pills nav-justified">
+                    <li class="active">
+                        <a href="/?page=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx">
+                            <i class="fa fa-search"></i>
+                            &nbsp;
+                            G&eacute;rer les utilisateurs
+                        </a>
+                    </li>
+                </ul>
+                <p></p>
+                <ul class="nav nav-pills nav-justified">
+                    <li class="active">
+                        <a href="/?page=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx">
+                            <i class="fa fa-search"></i>
+                            &nbsp;
+                            G&eacute;rer les ventes
+                        </a>
+                    </li>
+                </ul>
+                <p></p>
+                <ul class="nav nav-pills nav-justified">
+                    <li class="active">
+                        <a href="/?page=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx">
+                            <i class="fa fa-search"></i>
+                            &nbsp;
+                            G&eacute;rer la base de donn&eacute;es
+                        </a>
+                    </li>
+                </ul>
+                
+            </div>
+            
+        </div>
