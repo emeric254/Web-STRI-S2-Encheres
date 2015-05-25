d99cce7b24a8bb07469e4ea1337160a0a6ca34a6
emeric254 <emeric254@live.fr>
2015-05-25 21:51:24 +0200

    a test

--- PHP/core/accueil.php
+++ PHP/core/accueil.php
@@ -1,18 +1,23 @@
 <?php
 /* «nouveautes.php»
  * Page d'acceuil
- * 
+ *
  */
 
 include_once('core/class.php');
 include_once('core/model.php');
+
 include_once('vue/debut-accueil.php');
-$ListNouv=RecuperationDerniereVente(4);
-foreach($ListNouv as $idVente){
-	$vente = new Vente($idVente);
-	include('vue/resume-article.php');
-	unset($vente);
+
+$ListNouv = RecuperationDerniereVente(4);
+
+foreach($ListNouv as $idVente)
+{
+    $vente = new Vente($idVente);
+    include('vue/resume-article.php');
+    unset($vente);
 }
+
 include_once('vue/fin-contenu.php');
 
 
