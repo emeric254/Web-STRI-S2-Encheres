34c9c39ee731b8edd04cbba2d6db7d12b78658b4
emeric254 <emeric254@live.fr>
2015-05-25 23:43:12 +0200

    mise  en forme

--- PHP/index.php
+++ PHP/index.php
@@ -6,7 +6,9 @@
     if ( isset($_GET['page']) and !empty($_GET['page']) )
     {
         $page=htmlspecialchars($_GET['page']);
-    } else {
+    }
+    else
+    {
         $page="accueil"; //page par default
     }
 
@@ -14,14 +16,19 @@
 
     include("vue/entete.php");
     include("core/navbar.php");
-    if (isset($_GET['errMsg']) and !empty($_GET['errMsg'])) {
+
+    if (isset($_GET['errMsg']) and !empty($_GET['errMsg']))
+    {
         $errMsg = htmlspecialchars($_GET['errMsg']);
         include("vue/erreur.php"); //page d'erreur'
     }
+
     if(file_exists("core/$page.php"))
     {
         include_once("core/$page.php");
-    } else {
+    }
+    else
+    {
         $errMsg = "page introuvable";
         include("vue/erreur.php"); //page d'erreur'
     }
