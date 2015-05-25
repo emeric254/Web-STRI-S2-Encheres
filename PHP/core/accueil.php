eac65c6ac64d0134a5035e26e7dd5a9e34252294
Kriss <boulic.guillaume@gmail.com>
2015-05-25 17:45:00 +0200

    changement aceuill tendence a la place des nouveauté

--- PHP/core/accueil.php
+++ PHP/core/accueil.php
@@ -7,7 +7,7 @@
 include_once('core/class.php');
 include_once('core/model.php');
 include_once('vue/debut-accueil.php');
-$ListNouv=RecuperationDerniereVente(4);
+$ListNouv=RecuperationTendanceVente(4);
 foreach($ListNouv as $idVente){
 	$vente = new Vente($idVente);
 	include('vue/resume-article.php');
