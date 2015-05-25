b20cb919a6c3200ffcc57bc316fa8f646a97dae1
emeric254 <emeric254@live.fr>
2015-05-25 15:28:35 +0200

    maj gestion admin

---
+++ PHP/vue/admin/bdd.php
@@ -0,0 +1,17 @@
+<?php
+/* «bdd.php»
+ * page de gestion de la bdd
+ *
+ */
+?>
+        <!-- Titre -->
+        <div class="well">
+            <div class="container theme-showcase text-center" role="main" >
+<!--
+                @ TODO
+-->
+            </div>
+        </div>
+
+        <!-- Contenu -->
+        <div class="container">
