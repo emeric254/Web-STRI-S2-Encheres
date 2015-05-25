34746883d7b3d61c6ecf86b877f85f76f785de6b
emeric254 <emeric254@live.fr>
2015-05-25 10:03:39 +0200

    ajout resumé profil vue

---
+++ PHP/vue/resume-profil.php
@@ -0,0 +1,46 @@
+
+<?php
+/* «resume-profil.php»
+ * bout de page d'affichage du profil en résumé
+ *
+ *
+ * vars
+ *  > $profil    (objet "profil")
+ *
+ */
+?>
+            <div class="col-md-6">
+                <div class="panel panel-primary">
+                    <div class="panel-heading">
+                        <div class="btn-group pull-right">
+                            <a class="btn btn-default" href="http://google.fr">
+                                <i class="fa fa-code-fork"></i>
+                                Voir
+                            </a>
+                        </div>
+                        <h4>
+                            <?php print $profil->nom; ?>
+                            &nbsp;
+                            <?php print $profil->prenom; ?>
+                        </h4>
+                    </div>
+                    <div class="panel-body">
+                        <div class="col-md-6">
+                            <p class="pull-left">
+                                <img data-src="holder.js/200x200" class="img-thumbnail" alt="200x200" src="<?php print $profil->photo; ?>" data-holder-rendered="true" style="width: 200px; height: 200px;">
+                            </p>
+                        </div>
+                        <div class="col-md-6">
+                            <p>
+                                <?php print $profil->telephone; ?>
+                            </p>
+                            <p>
+                                <?php print $profil->email; ?>
+                            </p>
+                            <p>
+                                <?php print $profil->ville; ?>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
