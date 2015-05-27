425538f57a0c5300a37d3cba3dd4ec625778b8a3
remi <remi.b7@hotmail.fr>
2015-05-28 00:41:09 +0200

    a finir - acune vérif

--- PHP/core/encherir.php
+++ PHP/core/encherir.php
@@ -10,11 +10,24 @@ include_once('core/bdd.php');
 // Récupération des variables
 $erreur=0;
 $champErreur = " ";
+if (isset($_SESSION['id']) and !empty($_SESSION['id']))
+{
+    $idacheteur=$_SESSION['id'];
+}
+else
+{
+    $erreur=1;
+    $champErreur="il faut etre connecté pour enchérir";
+}
+
 if (isset($_POST['prix']) and !empty($_POST['prix'])) {
     $prix=htmlspecialchars($_POST['prix']);
 } else {
+    if($erreur){
+        $champErreur.=", ";
+    }
     $erreur=1;
-    $champErreur="Veuillez saisir un prix";
+    $champErreur="veuillez saisir un prix";
 }
  
 if (isset($_POST['id']) and !empty($_POST['id']) and $_POST['id'] >= 0)
@@ -24,7 +37,7 @@ if (isset($_POST['id']) and !empty($_POST['id']) and $_POST['id'] >= 0)
     if($erreur){
         $champErreur.=", ";
     }
-    $champErreur.="La vente choisie est incorrecte";
+    $champErreur.="la vente choisie est incorrecte";
     $erreur=1;
 }
 
@@ -38,4 +51,9 @@ if ($erreur==1)
 else
 {
     // on récupère les informations de l'annonce
-    $annonce = new Vente
+   // $annonce = new Vente($idvente);
+    
+    // TODO : faire des test sur le droit d'enchérir !!
+    if (DeposerEnchere($idvente, $idacheteur, $prix))
+    {
+        
