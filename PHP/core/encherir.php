466d19fb8006ff1799bca9cb2d650d806f033811
emeric254 <emeric254@live.fr>
2015-05-28 01:02:36 +0200

    test

--- PHP/core/encherir.php
+++ PHP/core/encherir.php
@@ -3,57 +3,57 @@
  * page de traitement pour l'ajout d'une enchère.
  *
  */
- 
+
 include_once('core/model.php');
 include_once('core/bdd.php');
- 
+
 // Récupération des variables
 $erreur=0;
-$champErreur = " ";
 if (isset($_SESSION['id']) and !empty($_SESSION['id']))
 {
     $idacheteur=$_SESSION['id'];
+
+    if (isset($_POST['id']) and !empty($_POST['id']) and $_POST['id'] >= 0)
+    {
+        $idvente=htmlspecialchars($_POST['id']);
+
+        if (isset($_POST['prix']) and !empty($_POST['prix']))
+        {
+            $prix=htmlspecialchars($_POST['prix']);
+            //~ test prix valable
+            //~
+        }
+        else
+        {
+            $erreur=1;
+            $errMsgg="veuillez saisir un prix";
+        }
+
+    }
+    else
+    {
+        $erreur=1;
+        $errMsgg.="la vente choisie est incorrecte";
+    }
 }
 else
 {
     $erreur=1;
-    $champErreur="il faut etre connecté pour enchérir";
-}
-
-if (isset($_POST['prix']) and !empty($_POST['prix'])) {
-    $prix=htmlspecialchars($_POST['prix']);
-} else {
-    if($erreur){
-        $champErreur.=", ";
-    }
-    $erreur=1;
-    $champErreur="veuillez saisir un prix";
-}
- 
-if (isset($_POST['id']) and !empty($_POST['id']) and $_POST['id'] >= 0)
-{
-    $idvente=htmlspecialchars($_POST['id']);
-} else {
-    if($erreur){
-        $champErreur.=", ";
-    }
-    $champErreur.="la vente choisie est incorrecte";
-    $erreur=1;
+    $errMsgg="il faut etre connecté pour enchérir";
 }
 
 
-if ($erreur==1)
+
+if($erreur==1)
 {
     $errMsgg="Veuillez vérifier les erreur suivantes : $champErreur.";
     include_once("vue/erreur.php");
-//    include_once("vue/vente.php
 }
 else
 {
     // on récupère les informations de l'annonce
    // $annonce = new Vente($idvente);
-    
+
     // TODO : faire des test sur le droit d'enchérir !!
     if (DeposerEnchere($idvente, $idacheteur, $prix))
     {
-        
