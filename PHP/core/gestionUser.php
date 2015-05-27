adb5036d5a8a16ec2cc62674b99c7e6ea4222a94
emeric254 <emeric254@live.fr>
2015-05-27 17:24:02 +0200

    ajout core admin

---
+++ PHP/core/gestionUser.php
@@ -0,0 +1,28 @@
+<?php
+/* «gestionUser.php»
+ *
+ *
+ * vars
+ *
+ */
+
+include_once('core/model.php');
+include_once('core/class.php');
+
+if(isset($_SESSION['id']) and !empty($_SESSION['id']))
+{
+    if (isset($_GET['action']) and !empty($_GET['action']))
+    {
+        $action = htmlspecialchars($_GET['action']);
+
+
+        ?>
+            <script>window.location="/?page=admin";</script>
+        <?php
+    }
+    else
+    {
+        include_once("vue/admin/debut-profils.php");
+    }
+}
+?>
