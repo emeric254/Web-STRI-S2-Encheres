3826a7d3e76e29c74451a562e2bd5e58ba1dc28a
emeric254 <emeric254@live.fr>
2015-05-29 00:01:54 +0200

    a cause du troll

--- PHP/core/model.php
+++ PHP/core/model.php
@@ -580,7 +580,7 @@ function recupCodePostalIdVille ($idVille)
     $req = "SELECT idville from ville WHERE codepostalville=?";
     $reqExec = $db->prepare($req);
     $retour = $reqExec->execute(array($idVille));
-
+    var_dump($retour);
     return($retour->fetch()['idville']);
 }
 
